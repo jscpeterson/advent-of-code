abc0f3805705344d74f03a23672122f999bc7514
Joseph Peterson <jscpeterson@gmail.com>
2020-12-02 07:57:22 -0700

    Alt day 1 solution using itertools

--- 2020/day1/day1.py
+++ 2020/day1/day1.py
@@ -1,22 +1,36 @@
+import time
+from itertools import combinations
+
 def get_data(filepath):
     with open(filepath) as f:
         return [int(line) for line in f]
 
+'''
 def part1(values):
     for v1 in values:
         for v2 in values:
             if v1+v2 == 2020:
                 return v1*v2
+'''
+
+def part1(values):
+    pairs = combinations(values, 2)
+    for pair in pairs:
+        if sum(pair) == 2020:
+            return pair[0]*pair[1]
 
 assert part1(get_data('test')) == 514579
-print(part1(get_data('input')))
+start_time = time.time()
+print('Part 1: %s' % part1(get_data('input')))
+print('Solved in %s seconds' % (time.time() - start_time))
 
 def part2(values):
-    for v1 in values:
-        for v2 in values:
-            for v3 in values:
-                if v1+v2+v3 == 2020:
-                    return v1*v2*v3
+    pairs = combinations(values, 3)
+    for pair in pairs:
+        if sum(pair) == 2020:
+            return pair[0]*pair[1]*pair[2]
 
 assert part2(get_data('test')) == 241861950
-print(part2(get_data('input')))
+start_time = time.time()
+print('\nPart 2: %s' % part2(get_data('input')))
+print('Solved in %s seconds' % (time.time() - start_time))
