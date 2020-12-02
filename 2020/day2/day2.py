15f2120ddd6d9d8ff8fcda341915bda6b28a9b83
Joseph Peterson <jscpeterson@gmail.com>
2020-12-02 08:47:42 -0700

    Day 2 solved

--- 2020/day2/day2.py
+++ 2020/day2/day2.py
@@ -1,16 +1,25 @@
-def is_valid(entry):
+def is_valid1(entry):
       policy, letter, password = entry.split(' ')
       letter = letter.strip(':')
       policy_lower, policy_upper = policy.split('-')
       return int(policy_lower)<=password.count(letter) and int(policy_upper)>=password.count(letter)
 
-def part1(filepath):
+def solve(filepath, validation_method):
     valid_passwords = 0
     entries = open(filepath).read().splitlines()
     for entry in entries:
-        if is_valid(entry):
+        if validation_method(entry):
            valid_passwords+=1
     return valid_passwords
 
-assert part1('test') == 2
-print('Part 1: %s' % part1('input'))
+assert solve('test', is_valid1) == 2
+print('Part 1: %s' % solve('input', is_valid1))
+
+def is_valid2(entry):
+      policy, letter, password = entry.split(' ')
+      letter = letter.strip(':')
+      policy_index1, policy_index2 = policy.split('-')
+      return (password[int(policy_index1)-1] == letter) ^ (password[int(policy_index2)-1] == letter)
+
+assert solve('test', is_valid2) == 1
+print('Part 2: %s' % solve('input', is_valid2))
