18593be9c93b74430c155e382511a259947208d3
Joseph Peterson <jscpeterson@gmail.com>
2020-12-03 16:27:13 -0700

    ocd refactoring

--- 2020/03/day3.py
+++ 2020/03/day3.py
@@ -1,4 +1,12 @@
 ORIGIN = (0, 0)
+PART_1_SLOPE = (3, 1)
+PART_1_TEST = 7
+PART_2_SLOPES = [(1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)]
+PART_2_TEST = 336
 
 def get_data(filepath):
     with open(filepath) as f:
@@ -10,7 +18,7 @@ def solve(grid, x_speed, y_speed):
     max_y = len(grid)
     location = ORIGIN
     for line in grid:
-        x = (location[0] + x_speed) % len(grid[0])
+        x = (location[0] + x_speed) % max_x
         y = location[1] + y_speed
         if y >= max_y:
             return trees
@@ -22,25 +30,15 @@ def solve(grid, x_speed, y_speed):
             raise Exception('unknown terrain: %s' % grid[y][x])
         location = (x, y)
 
-SLOPE = (3, 1)
+assert solve(get_data('test'), PART_1_SLOPE[0], PART_1_SLOPE[1]) == PART_1_TEST
+print('Part 1: %s' % solve(get_data('input'), PART_1_SLOPE[0], PART_1_SLOPE[1]))
 
-assert solve(get_data('test'), SLOPE[0], SLOPE[1]) == 7
-print('Part 1: %s' % solve(get_data('input'), SLOPE[0], SLOPE[1]))
+def solve2(grid, slopes): 
+    if slopes:
+       slope = slopes[0]
+       return solve(grid, slope[0], slope[1]) * solve2(grid, slopes[1:])
+    else:
+       return 1 
 
-def solve2(filepath): 
-    grid = get_data(filepath)
-
-    SLOPE_0 = (1, 1)
-    SLOPE_1 = (3, 1)
-    SLOPE_2 = (5, 1)
-    SLOPE_3 = (7, 1)
-    SLOPE_4 = (1, 2)
-    
-    return solve(grid, SLOPE_0[0], SLOPE_0[1]) * \
-           solve(grid, SLOPE_1[0], SLOPE_1[1]) * \
-           solve(grid, SLOPE_2[0], SLOPE_2[1]) * \
-           solve(grid, SLOPE_3[0], SLOPE_3[1]) * \
-           solve(grid, SLOPE_4[0], SLOPE_4[1])
-
-assert solve2('test') == 336
-print('Part 2: %s' % solve2('input'))
+assert solve2(get_data('test'), PART_2_SLOPES) == PART_2_TEST
+print('Part 2: %s' % solve2(get_data('input'), PART_2_SLOPES))
