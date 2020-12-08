80e2d9bc71c61f6dd9199b3d5d2e4e820636bd9c
Joseph Peterson <jscpeterson@gmail.com>
2020-12-07 23:56:42 -0700

    2020/08 solved

--- 2020/08/day8.py
+++ 2020/08/day8.py
@@ -3,22 +3,48 @@ JUMP = 'jmp' # jmp jumps to a new instruction relative to itself, the next instr
 NO_OPERATION = 'nop' # operation immediately below is executed next
 POS = '+'
 NEG = '-'
+MAX_RUN_BEFORE_TIMEOUT = 2
 
 def get_data(filepath):
     return [line.strip() for line in open(filepath).readlines()]
 
-def compute(instructions, debug = False):
+def compute(instructions, debug1 = False, debug2 = False):
+    init_instructions = instructions.copy()
     accumulator = 0
     i = 0
-    if debug:
+    if debug1:
         instructions_run = set()
+    if debug2:
+        instructions_run = list()
+        instructions_changed = set()
+        instructions = init_instructions
     running = True
     while running:
-        if debug:
+        if i == len(instructions):
+            return accumulator
+        if debug1:
             if i in instructions_run:
                 return accumulator
             else:
-                instructions_run.add(i) 
+                instructions_run.add(i)
+        if debug2:
+            if instructions_run.count(i) >= MAX_RUN_BEFORE_TIMEOUT:
+                i = 0
+                accumulator = 0
+                instructions = init_instructions.copy()
+                instructions_run = list()
+                for i2, instruction in enumerate(instructions):
+                    if i2 not in instructions_changed:
+                        if JUMP in instruction:
+                            instructions[i2] = instruction.replace(JUMP, NO_OPERATION)
+                            instructions_changed.add(i2)
+                            break
+                        elif NO_OPERATION in instruction:
+                            instructions[i2] = instruction.replace(NO_OPERATION, JUMP)
+                            instructions_changed.add(i2)
+                            break
+            else:
+                instructions_run.append(i)
         operation, argument = instructions[i].split(' ')
         sign = argument[0]
         value = int(argument[1:])
@@ -31,5 +57,8 @@ def compute(instructions, debug = False):
             continue
         i += 1
 
-assert compute(get_data('test'), debug = True) == 5
-print('Part 1: %d' % compute(get_data('input'), debug = True))
+assert compute(get_data('test'), debug1 = True) == 5
+print('Part 1: %d' % compute(get_data('input'), debug1 = True))
+
+assert compute(get_data('test'), debug2 = True) == 8
+print('Part 1: %d' % compute(get_data('input'), debug2 = True))
