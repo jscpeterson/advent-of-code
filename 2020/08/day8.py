56082109ee8bf0a184f6255ab927d96266f97e49
Joseph Peterson <jscpeterson@gmail.com>
2020-12-08 11:17:32 -0700

    Clean up compute function, infinite loop throws custom exception

--- 2020/08/day8.py
+++ 2020/08/day8.py
@@ -4,44 +4,27 @@ NO_OPERATION = 'nop' # operation immediately below is executed next
 POS = '+'
 NEG = '-'
 
+
 def get_data(filepath):
     return [line.strip() for line in open(filepath).readlines()]
 
-def compute(instructions, debug1 = False, debug2 = False):
-    init_instructions = instructions.copy()
+
+class InfiniteLoopException(Exception):
+    pass
+
+
+def compute(instructions):
     accumulator = 0
     i = 0
-    if debug1 or debug2:
-        instructions_run = set()
-    if debug2:
-        instructions_changed = set()
+    instructions_run = set()
     running = True
     while running:
         if i == len(instructions):
             return accumulator
-        if debug1:
-            if i in instructions_run:
-                return accumulator
-            else:
-                instructions_run.add(i)
-        if debug2:
-            if i in instructions_run:
-                i = 0
-                accumulator = 0
-                instructions = init_instructions.copy()
-                instructions_run = set()
-                for i2, instruction in enumerate(instructions):
-                    if i2 not in instructions_changed:
-                        if JUMP in instruction:
-                            instructions[i2] = instruction.replace(JUMP, NO_OPERATION)
-                            instructions_changed.add(i2)
-                            break
-                        elif NO_OPERATION in instruction:
-                            instructions[i2] = instruction.replace(NO_OPERATION, JUMP)
-                            instructions_changed.add(i2)
-                            break
-            else:
-                instructions_run.add(i)
+        if i in instructions_run:
+            raise InfiniteLoopException({ACC: accumulator})
+        else:
+            instructions_run.add(i)
         operation, argument = instructions[i].split(' ')
         sign = argument[0]
         value = int(argument[1:])
@@ -54,8 +37,40 @@ def compute(instructions, debug1 = False, debug2 = False):
             continue
         i += 1
 
-assert compute(get_data('test'), debug1 = True) == 5
-print('Part 1: %d' % compute(get_data('input'), debug1 = True))
 
-assert compute(get_data('test'), debug2 = True) == 8
-print('Part 1: %d' % compute(get_data('input'), debug2 = True))
+def solve1(filepath):
+    instructions = get_data(filepath)
+    try:
+        compute(instructions)
+    except InfiniteLoopException as e:
+        return e.args[0][ACC]
+
+
+def solve2(filepath, instructions_changed=None):
+    if not instructions_changed:
+        instructions_changed = set()
+    first_instructions = get_data(filepath)
+    debugging = True
+    new_instructions = first_instructions[:]
+    while debugging:
+        try:
+            return compute(new_instructions)
+        except InfiniteLoopException as e:
+            new_instructions = first_instructions[:]
+            for i2, instruction in enumerate(first_instructions):
+                if i2 not in instructions_changed:
+                    if JUMP in instruction:
+                        new_instructions[i2] = instruction.replace(JUMP, NO_OPERATION)
+                        instructions_changed.add(i2)
+                        break
+                    elif NO_OPERATION in instruction:
+                        new_instructions[i2] = instruction.replace(NO_OPERATION, JUMP)
+                        instructions_changed.add(i2)
+                        break
+
+
+assert solve1('test') == 5
+print('Part 1: %d' % solve1('input'))
+
+assert solve2('test') == 8
+print('Part 1: %d' % solve2('input'))
