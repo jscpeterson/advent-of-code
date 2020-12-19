e4989f6d6baffe3d5aa5e3d1fc07070e13abe1ed
Joseph Peterson <jscpeterson@gmail.com>
2020-12-18 20:28:22 -0700

    2020/18 solved

--- 2020/18/day18.py
+++ 2020/18/day18.py
@@ -6,16 +6,13 @@ ANSI_SILVER = '\033[1;37m'
 ANSI_GOLD = '\033[1;33m'
 ANSI_NC = '\033[0;m'    
 DIGITS = '0123456789'
-OPERATORS = '*+'
-OPERATOR_PRIORITIES = {'(': lambda tokens, pointer: calculate_gold(tokens=tokens[int(pointer)+1:]),
-                       '*': lambda tokens, pointer: tokens[int(pointer)-1] * tokens[int(pointer)+1],
-                       '+': lambda tokens, pointer: tokens[int(pointer)-1] + tokens[int(pointer)+1]}
+OPERATORS = '+*'
 SILVER_TESTS = {'1+(2*3)+(4*(5+6))': 51,
                 '2*3+(4*5)': 26,
                 '5+(8*3+9+3*4*3)': 437,
                 '5*9*(7*3*3+9*3+(8+6*4))': 12240,
                 '((2+4*9)*(6+9*8+6)+6)+2+4*2': 13632}
-GOLD_TESTS = {'1+(2 * 3)+(4*(5+6))': 51,
+GOLD_TESTS = {'1+(2*3)+(4*(5+6))': 51,
               '2*3+(4*5)': 46,
               '5+(8*3+9+3*4*3)': 1445,
               '5*9*(7*3*3+9*3+(8+6*4))': 669060,
@@ -27,6 +24,31 @@ def get_data(filepath):
 def solve(calculation_method, filepath):
     expressions = get_data(filepath)
     return sum(calculation_method(expression) for expression in expressions)
+
+def calculate_silver(expression=None, tokens=None):
+    if expression and not tokens:
+        tokens = [token for token in expression]
+    operands = list()
+    operators = list()
+    while tokens:
+        token = tokens.pop(0)
+        if token in DIGITS:
+            operands.append(int(token))
+        elif token in OPERATORS:
+            operators.append(token)
+        elif token in '(':
+            operands.append(calculate_silver(tokens=tokens))
+        elif token in ')':
+            break
+    while operators:
+        operand1 = operands.pop(0)
+        operator = operators.pop(0)
+        operand2 = operands.pop(0)
+        if operator == '+':
+            operands.insert(0, operand1+operand2)
+        elif operator == '*':
+            operands.insert(0, operand1*operand2)
+    return operands[0]
 
 def calculate_gold(expression=None, tokens=None):
     if expression and not tokens:
@@ -45,41 +67,22 @@ def calculate_gold(expression=None, tokens=None):
         elif token in ')':
             break
         priority_level += 1
-    while operators:
-        operand1 = operands.pop(0)
-        operator = operators.pop(0)
-        operand2 = operands.pop(0)
-        if operator == '+':
-            operands.insert(0, operand1+operand2)
-        elif operator == '*':
-            operands.insert(0, operand1*operand2)
-    return operands[0]
-
-def calculate_silver(expression=None, tokens=None):
-    if expression and not tokens:
-        tokens = [token for token in expression]
-    operands = list()
-    operators = list()
     priority_level = 0
-    while tokens:
-        token = tokens.pop(0)
-        if token in DIGITS:
-            operands.append(int(token))
-        elif token in OPERATORS:
-            operators.append(token)
-        elif token in '(':
-            operands.append(calculate_gold(tokens=tokens))
-        elif token in ')':
-            break
-        priority_level += 1
     while operators:
-        operand1 = operands.pop(0)
-        operator = operators.pop(0)
-        operand2 = operands.pop(0)
-        if operator == '+':
-            operands.insert(0, operand1+operand2)
-        elif operator == '*':
-            operands.insert(0, operand1*operand2)
+        if OPERATORS[priority_level] not in operators:
+            priority_level += 1
+            continue
+        if OPERATORS[priority_level] == operators[0]:
+            operand1 = operands.pop(0)
+            operator = operators.pop(0)
+            operand2 = operands.pop(0)
+            if operator == '+':
+                operands.insert(0, operand1+operand2)
+            elif operator == '*':
+                operands.insert(0, operand1*operand2)
+        else:
+            operands.append(operands.pop(0))
+            operators.append(operators.pop(0))
     return operands[0]
 
 def test_calculate(calculate_method, test_cases):
@@ -90,9 +93,9 @@ def results(solution_method_1, solution_method_2):
     start_time = time.time()
     print('Part 1: %s%d%s' % (ANSI_SILVER, solve(solution_method_1, SOLUTION_INPUT), ANSI_NC))
     print('Solved in %s seconds' % (time.time() - start_time))
-    #assert test_calculate(solution_method_2, GOLD_TESTS)
-    #start_time = time.time()
-    #print('Part 2: %s%d%s' % (ANSI_GOLD, solve(solution_method_2, SOLUTION_INPUT), ANSI_NC))
-    #print('Solved in %s seconds' % (time.time() - start_time))
+    assert test_calculate(solution_method_2, GOLD_TESTS)
+    start_time = time.time()
+    print('Part 2: %s%d%s' % (ANSI_GOLD, solve(solution_method_2, SOLUTION_INPUT), ANSI_NC))
+    print('Solved in %s seconds' % (time.time() - start_time))
 
 results(calculate_silver, calculate_gold)
