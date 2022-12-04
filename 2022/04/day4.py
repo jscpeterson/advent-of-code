85d2490de65d7a069413230265177ecfcaaaec95
jscpeterson <jscpeterson@gmail.com>
2022-12-04 09:16:39 -0700

    Day 4 way better solution

--- 2022/04/day4.py
+++ 2022/04/day4.py
@@ -1,24 +1,13 @@
+import re
+
 data = [line.strip('\n') for line in open('input').readlines()]
 
-def does_contain(range1, range2):
-    return all(section in range2 for section in range1)
- 
-def any_contain(range1, range2):
-    return any(section in range2 for section in range1)
-
-silver = 0
-gold = 0
+sections_that_contain = 0
+sections_with_overlap = 0
 for line in data:
-    assignments = line.split(',')
-    ass_ranges = []
-    for assignment in assignments:
-        (lower_bound, upper_bound) = [int(bound) for bound in assignment.split('-')]
-        ass_ranges.append(range(lower_bound, upper_bound+1))
-    (ass_range1, ass_range2) = ass_ranges
-    does_cont = does_contain(ass_range1, ass_range2) or does_contain(ass_range2, ass_range1)
-    any_cont = any_contain(ass_range1, ass_range2)
-    if does_cont:
-        silver += 1
-    if any_cont:
-        gold += 1
-print(f"Silver: {silver}\nGold: {gold}")
+    groups = re.match("(\d+)-(\d+),(\d+)-(\d+)", line).groups()
+    first_lower_bound, first_upper_bound, second_lower_bound, second_upper_bound = (int(group) for group in groups)
+    sections_that_contain += (first_lower_bound >= second_lower_bound and first_upper_bound <= second_upper_bound) or  (second_lower_bound >= first_lower_bound and second_upper_bound <= first_upper_bound)
+    sections_with_overlap += first_lower_bound <= second_upper_bound and second_lower_bound <= first_upper_bound
+
+print(f"Silver: {sections_that_contain}\nGold: {sections_with_overlap}")
