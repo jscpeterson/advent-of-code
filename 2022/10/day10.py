43cfaddde0fc9cb39882567ffbce3802b3dd925c
jscpeterson <jscpeterson@gmail.com>
2022-12-10 13:35:46 -0700

    Day 10 part 2 solved

--- 2022/10/day10.py
+++ 2022/10/day10.py
@@ -10,8 +10,13 @@ class CPU:
    signal_strength = 0
    signal_strengths = dict() # Cycles: signal_strength
 
-   def __main__(display_width=40, display_height=6):
-       pass
+   pixels = []
+   pixel_being_drawn = 0
+
+   def __init__(self, display_width=40, display_height=6):    
+       self.display_width = display_width
+       self.display_height = display_height
+       self.pixels = ["."] * self.display_width * self.display_height
 
    def get_signal_strength_sums(self):
        return sum(self.signal_strengths.values())
@@ -23,6 +28,18 @@ class CPU:
        if self.cycles == 20 or (self.cycles + 20) % 40 == 0:
            self.signal_strength = self.cycles * self.x
            self.signal_strengths[self.cycles] = self.signal_strength
+
+       # Draw pixel
+       pixel_being_drawn = (self.cycles-1) % self.display_width
+       sprite_in_x_register = (self.x-1, self.x, self.x+1)
+       if pixel_being_drawn in sprite_in_x_register:
+           self.pixels[self.cycles-1] = '#'
+
+   def display(self):
+       for location, pixel in enumerate(self.pixels):
+           sys.stdout.write(pixel)
+           if (location + 1) % self.display_width == 0:
+                sys.stdout.write('\n')
 
    def run(self, program_instructions):
        for instruction in program_instructions:
@@ -48,4 +65,5 @@ program_instructions = [line.strip("\n") for line in open(filename).readlines()]
 device = CPU()
 device.run(program_instructions)
 print(f"Silver: {device.get_signal_strength_sums()}")
-print(f"Gold: ??? ")
+print(f"Gold: ")
+device.display()
